70716560f922725f9fd9fbf4d6612a3d7c29b4c8
BrightDN <123126129+BrightDN@users.noreply.github.com>
2023-09-04 17:27:06 +0200

    Update and rename testMod.mjs to mProjectData.mjs

---
+++ mProjectData.mjs
@@ -0,0 +1,29 @@
+export default function getWebProjectsData(){
+    this.getData = setWebProjectData();
+    return getData;
+}
+
+function setWebProjectData(){
+    const dataSetter= [
+        {
+            "type":"webproject",
+            "name":"Pokemon Bingo",
+            "thumbnail":"null",
+            "websiteLink":"https://brightdn.github.io/pokeTools/bingo.html",
+            "codeLink":"https://github.com/BrightDN/pokeTools/blob/main/bingo.html",
+            "usedFiles":"null",
+            "relatedProjects":"null"
+        },
+        {
+            "type":"webproject",
+            "name":"Store Manager",
+            "thumbnail":"null",
+            "websiteLink":"null",
+            "codeLink":"null",
+            "usedFiles":"null",
+            "relatedProjects":"null"
+        }
+                    ]
+    
+    return dataSetter;
+}
